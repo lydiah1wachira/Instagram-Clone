a3a5d3a8df5808e303b5327f907ba3737bd876db
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-07 00:47:39 +0300

    deleted the previous app.

--- instagram/settings.py
+++ instagram/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'insta.apps.IgConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'bootstrap5',
     'django_registration',
     'tinymce',
+    
 ]
 
 MIDDLEWARE = [
