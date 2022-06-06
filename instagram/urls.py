a29ef27c4212ef4d04ce74cad8d8f0db6fe848e2
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-07 01:04:51 +0300

    connect app's URLconf to the project.

--- instagram/urls.py
+++ instagram/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('insta/', include('insta.urls')),
     path('accounts/', include('django_registration.backends.one_step.urls')),
 ]
