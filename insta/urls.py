bda613be583c48dafd62e624e787f9c4ebbea308
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-08 00:38:45 +0300

    create a URLpattern for the search results view.

--- insta/urls.py
+++ insta/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-  path('', views.landing, name='landing' )
+  path('', views.landing, name='landing' ),
+  path('search/', views.search_results, name='search_results'),
 ]
