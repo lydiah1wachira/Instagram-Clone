9a848f16e25149779b4524a3608b269347bc4929
lydiah1wachira <lydiahcedric@gmail.com>
2022-06-07 12:41:19 +0300

    create view function for the landing page.

--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
+from models import Post,Profile
+from django.contrib.auth.models import User
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def landing(request):
+  '''
+  view function to display the landing page and all of its data
+  '''
+  posts = Post.get_images().order_by('-date_posted')
+  users = User.objects.exclude(id=request.user.id)
+  current_user = request.user
+  suggested_accounts = Profile.objects.all()
+
+  return render(request, 'landing-page.html', {'posts':posts, 'users':users, 'current_user':current_user, 'suggested_accounts':suggested_accounts})
